5128754a7bda99f26996bff79f773d6e5771dffe
fatihserhatturan <95492309+fatihserhatturan@users.noreply.github.com>
2023-09-25 20:44:09 +0300

    güncelleme

--- WeatherForecast.cs
+++ WeatherForecast.cs
@@ -6,6 +6,7 @@ namespace AuthServer.API
     {
         public DateTime Date { get; set; }
 
+        public int id { get; set; }
         public int TemperatureC { get; set; }
 
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
