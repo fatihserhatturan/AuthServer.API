fa4de0c6c6889fccecb07701580e1734cbe12b53
fatih serhat turan <95492309+fatihserhatturan@users.noreply.github.com>
2023-12-06 12:41:46 +0300

    Update InvoiceController.cs

--- MiniApp2.API/Controllers/InvoiceController.cs
+++ MiniApp2.API/Controllers/InvoiceController.cs
@@ -21,9 +21,6 @@ namespace MiniApp2.API.Controllers
 
             var userIdClaim = User.Claims.FirstOrDefault(x => x.Type == ClaimTypes.NameIdentifier);
 
-            //veri tabanında  userId veya userName alanları üzerinden gerekli dataları çek
-
-            // stockId stockQuantity  Category  UserId/UserName
 
             return Ok($"Invoice işlemleri =>  UserName: {userName }- UserId:{userIdClaim.Value}");
         }
